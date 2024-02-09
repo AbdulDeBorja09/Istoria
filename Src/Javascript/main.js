28476b82d24d4abbdc6ed25c064d9ad6676cae45
AbdulDeBorja09 <abduldb09@gmail.com>
2024-02-10 07:03:59 +0800

    day1

--- Src/Javascript/main.js
+++ Src/Javascript/main.js
@@ -1,20 +1,39 @@
-// add hovered class to selected list item
-let list = document.querySelectorAll(".navigation li");
-function activeLink() {
-  list.forEach((item) => {
-    item.classList.remove("hovered");
-  });
-  this.classList.add("hovered");
+// // add hovered class to selected list item
+// let list = document.querySelectorAll(".navigation li");
+// function activeLink() {
+//   list.forEach((item) => {
+//     item.classList.remove("hovered");
+//   });
+//   this.classList.add("hovered");
+// }
+
+// list.forEach((item) => item.addEventListener("mouseover", activeLink));
+
+// // Menu Toggle
+// let toggle = document.querySelector(".toggle");
+// let navigation = document.querySelector(".navigation");
+// let main = document.querySelector(".main");
+
+// toggle.onclick = function () {
+//   navigation.classList.toggle("active");
+//   main.classList.toggle("active");
+// };
+
+function updateTime() {
+  var now = new Date();
+  var hours = now.getHours();
+  var minutes = now.getMinutes();
+  var seconds = now.getSeconds();
+
+  // Add leading zeros if necessary
+  hours = (hours < 10 ? "0" : "") + hours;
+  minutes = (minutes < 10 ? "0" : "") + minutes;
+  seconds = (seconds < 10 ? "0" : "") + seconds;
+
+  var timeString = hours + ":" + minutes + ":" + seconds;
+
+  document.getElementById("time").textContent = timeString;
 }
 
-list.forEach((item) => item.addEventListener("mouseover", activeLink));
-
-// Menu Toggle
-let toggle = document.querySelector(".toggle");
-let navigation = document.querySelector(".navigation");
-let main = document.querySelector(".main");
-
-toggle.onclick = function () {
-  navigation.classList.toggle("active");
-  main.classList.toggle("active");
-};
+// Update time every second
+setInterval(updateTime, 1000);
